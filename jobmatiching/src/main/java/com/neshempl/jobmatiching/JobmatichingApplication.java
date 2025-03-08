7ed1bd5fb15b6760182ee6d282a1e0b9f3878710
neshvig10 <s.vigneshwaran.cse21@iitbhu.ac.in>
2025-03-08 15:26:23 +0530

    Files initiated

---
+++ jobmatiching/src/main/java/com/neshempl/jobmatiching/JobmatichingApplication.java
@@ -0,0 +1,13 @@
+package com.neshempl.jobmatiching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobmatichingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobmatichingApplication.class, args);
+	}
+
+}
