d4bd26bb7ea36ca5d283410818c8ca5bcdddf3e7
neshvig10 <s.vigneshwaran.cse21@iitbhu.ac.in>
2025-03-15 13:21:19 +0530

    api gateway initiated

--- usermanagement/src/main/java/com/neshempl/usermanagement/controller/AuthController.java
+++ usermanagement/src/main/java/com/neshempl/usermanagement/controller/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RequestMapping("/api/auth")
+@RequestMapping("/usermanagement/api/auth")
 @RestController
 public class AuthController {
 
--- usermanagement/src/main/java/com/neshempl/usermanagement/controller/UserController.java
+++ usermanagement/src/main/java/com/neshempl/usermanagement/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
-@RequestMapping("/api/user")
+@RequestMapping("/usermanagement/api/user")
 @RestController
 public class UserController {
 
