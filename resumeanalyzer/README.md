ad0cc9edb42aebb20aa6689ab6a04c1975710cc3
neshvig10 <svigneshwaran1073@gmail.com>
2025-04-07 20:17:01 +0530

    resume analyzer functions

---
+++ resumeanalyzer/README.md
@@ -0,0 +1,4 @@
+### To Run this server 
+
+    cd app
+    uvicorn main:app --reload
--- resumeanalyzer/app/main.py
+++ resumeanalyzer/app/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
 from middlewares import add_middlewares
+from router import routes
 
 app = FastAPI()
 
+app.include_router(routes.router)
 add_middlewares(app)
-
-
-@app.post("/api/resume/analyze")
-async def getfirstpage(resumeId):
-    
-    return {"resumeId" : resumeId}
     
